18707f7872b3f7f8e7c08a54c689a22bf416ab20
Konrad <konradlindenberg@gmail.com>
2026-01-19 13:13:35 +0100

    WS now handles clients connecting and disconnecting. Further work needed

--- public/scripts/code.js
+++ public/scripts/code.js
@@ -1,1 +1,27 @@
-console.log("hello")
+// DOM elements
+// ------------------------------------------------------------
+const form = document.querySelector("form");
+const msgElement = document.querySelector("input#msg");
+const chatBoxElement = document.querySelector("div#chatBox");
+
+// dependencies Websocket
+// ------------------------------------------------------------
+const websocket = new WebSocket("ws://localhost:3000")
+
+// variabler
+// ------------------------------------------------------------
+
+// event handlers/listeners
+// ------------------------------------------------------------
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Prevented form submit");
+});
+
+// is typing?
+// msgElement.addEventListener("keydown", (e) => {
+//     console.log("User is typing...")
+// })
+
+// funktioner
+// ------------------------------------------------------------
--- server.js
+++ server.js
@@ -4,21 +4,52 @@
 // Express import för server app
 // ------------------------------------------------------------
 import express from "express";
-
+import http from "http";
+import { WebSocketServer } from "ws";
 // miljövariabler
 // ------------------------------------------------------------
 const app = express();
 app.use(express.static("public"));
 const PORT = 3000;
 
+// http server
+const server = http.createServer(app);
+
+// WS server
+const wss = new WebSocketServer({ noServer: true });
+
+// Handskakning av WS connection
+server.on("upgrade", (req, socket, head) => {
+  console.log("event upgreade...");
+
+  wss.handleUpgrade(req, socket, head, (ws) => {
+    console.log("Client:", req.headers["user-agent"]);
+
+    wss.emit("connection", ws, req);
+  });
+});
+
 // middleware
 // ------------------------------------------------------------
 
 // event handlers
 // ------------------------------------------------------------
+wss.on("connection", (ws) => {
+  // Loggar anslutande klienter. Ska även visa när klienter lämnar
+  console.log(`New client connected, total clients: ${wss.clients.size}`);
+  ws.on("close", () => {
+    console.log(`Client disconnected, total clients: ${wss.clients.size}`);
+  });
+});
+
+const obj = {
+  type: "new_client",
+  msg: "New client connected",
+};
+// broadcast(wss, obj);
 
 // server start
 // ------------------------------------------------------------
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
